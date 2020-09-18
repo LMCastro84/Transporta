c81a31425cf8aacb8562a82d124531397ee6d108
Castro <luisc@Castro-Desktop>
2020-09-18 22:26:44 +0100

    Interface Operacoes + abstract methods

---
+++ ProjectoBancoRumos02/src/com/banco/operacoes/Operacoes.java
@@ -0,0 +1,13 @@
+package com.banco.operacoes;
+
+public interface Operacoes {
+
+    public void depositar();
+
+    public void levantar();
+
+    public void transferir();
+
+    public void mostrarSaldo();
+
+}
