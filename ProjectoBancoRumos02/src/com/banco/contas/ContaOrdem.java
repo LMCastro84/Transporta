c32dc90726ffa2c187dcd6eb59afc6782c905629
Castro <luisc@Castro-Desktop>
2020-09-18 22:17:48 +0100

    Class ContaOrdem extends Conta + Constructor

---
+++ ProjectoBancoRumos02/src/com/banco/contas/ContaOrdem.java
@@ -0,0 +1,10 @@
+package com.banco.contas;
+
+public class ContaOrdem extends Conta{
+    
+    public ContaOrdem(int numConta, double saldo) {
+        super(numConta, saldo);
+    }
+    
+    
+}
