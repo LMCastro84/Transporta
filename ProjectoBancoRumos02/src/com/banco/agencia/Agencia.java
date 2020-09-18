3d0ef67f0ae42f22ee4c7d7c7f0733e1aa099bf5
Castro <luisc@Castro-Desktop>
2020-09-18 21:37:05 +0100

    New Menu class instance

--- ProjectoBancoRumos02/src/com/banco/agencia/Agencia.java
+++ ProjectoBancoRumos02/src/com/banco/agencia/Agencia.java
@@ -8,6 +8,8 @@ public class Agencia {
     private int numAgencia;
     private String moradaAgencia;
     public List<Cliente> clientes ArrayList<>();
+    
+    Menu menu = new Menu();
 
     public Agencia(int numAgencia, String moradaAgencia, List<Cliente> clientes) {
         this.numAgencia = numAgencia;
