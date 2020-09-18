9df5363e2fb1a3b12e76c8fbe124ed1582262b11
Castro <luisc@Castro-Desktop>
2020-09-18 22:27:43 +0100

    implements Operacoes + methods

---
+++ ProjectoBancoRumos02/src/com/banco/cartoes/CartaoBancario.java
@@ -0,0 +1,27 @@
+package com.banco.cartoes;
+
+import com.banco.operacoes.Operacoes;
+
+public class CartaoBancario implements Operacoes {
+
+    @Override
+    public void depositar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void levantar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void transferir() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mostrarSaldo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
