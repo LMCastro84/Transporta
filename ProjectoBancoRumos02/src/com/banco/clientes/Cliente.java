899ef2139c2b89ac245fda04f624599ec8ffeee8
Castro <luisc@Castro-Desktop>
2020-09-18 21:55:39 +0100

    Constructors

--- ProjectoBancoRumos02/src/com/banco/clientes/Cliente.java
+++ ProjectoBancoRumos02/src/com/banco/clientes/Cliente.java
@@ -12,4 +12,19 @@ public class Cliente {
     private String telefone;
     private String email;
     private int numAgencia;
+
+    public Cliente(){}
+    
+    public Cliente(int numCliente, long numCidadao, String nome, String morada, String profissao, String telefone, String email, int numAgencia) {
+        this.numCliente = numCliente;
+        this.numCidadao = numCidadao;
+        this.nome = nome;
+        this.morada = morada;
+        this.profissao = profissao;
+        this.telefone = telefone;
+        this.email = email;
+        this.numAgencia = numAgencia;
+    }
+    
+    
 }
