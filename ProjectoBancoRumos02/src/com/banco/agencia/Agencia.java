b647329f973ea95769f2a75916910b366a1b7b4c
Castro <luisc@Castro-Desktop>
2020-09-18 21:39:35 +0100

    Getters + Setters

--- ProjectoBancoRumos02/src/com/banco/agencia/Agencia.java
+++ ProjectoBancoRumos02/src/com/banco/agencia/Agencia.java
@@ -16,10 +16,30 @@ public class Agencia {
         this.moradaAgencia = moradaAgencia;
         this.clientes = clientes;
     }
+
+    public void setNumAgencia(int numAgencia) {
+        this.numAgencia = numAgencia;
+    }
+
+    public void setMoradaAgencia(String moradaAgencia) {
+        this.moradaAgencia = moradaAgencia;
+    }
+
+    public int getNumAgencia() {
+        return numAgencia;
+    }
+
+    public String getMoradaAgencia() {
+        return moradaAgencia;
+    }
+    
+    
     
     public Cliente criarCliente (){
         return null;
     }
     
     public void eliminarCliente(){}
+    
+    
 }
