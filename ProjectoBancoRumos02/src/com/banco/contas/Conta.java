e04f6e9ea46e802ae494062b2c08a7767b916072
Castro <luisc@Castro-Desktop>
2020-09-18 22:14:35 +0100

    numConta + saldo + Constructor

--- ProjectoBancoRumos02/src/com/banco/contas/Conta.java
+++ ProjectoBancoRumos02/src/com/banco/contas/Conta.java
@@ -1,5 +1,14 @@
 package com.banco.contas;
 
 public class Conta {
+
+    private int numConta;
+    private double saldo;
+
+    public Conta(int numConta, double saldo) {
+        this.numConta = numConta;
+        this.saldo = saldo;
+    }
+    
     
 }
