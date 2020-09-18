3da682a31fd48c9727b79c9f5ef32746e9b95616
Castro <luisc@Castro-Desktop>
2020-09-18 22:06:17 +0100

    Constructor

--- ProjectoBancoRumos02/src/com/banco/clientes/ClienteNormal.java
+++ ProjectoBancoRumos02/src/com/banco/clientes/ClienteNormal.java
@@ -1,5 +1,11 @@
 package com.banco.clientes;
 
-public class ClienteNormal {
+public class ClienteNormal extends Cliente {
+
+    public ClienteNormal(int numCliente, long numCidadao, String nome, String morada, String profissao, String telefone, String email, int numAgencia) {
+        super(numCliente, numCidadao, nome, morada, profissao, telefone, email, numAgencia);
+    }
     
+    
+
 }
