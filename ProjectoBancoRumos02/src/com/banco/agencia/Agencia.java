13f3988310455b4ac5f622f4563198f6f43d6934
Castro <luisc@Castro-Desktop>
2020-09-18 21:34:50 +0100

    numAgencia + moradaAgencia + clientes

--- ProjectoBancoRumos02/src/com/banco/agencia/Agencia.java
+++ ProjectoBancoRumos02/src/com/banco/agencia/Agencia.java
@@ -1,5 +1,11 @@
 package com.banco.agencia;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Agencia {
 
+    private int numAgencia;
+    private String moradaAgencia;
+    public List<Cliente> clientes ArrayList<>();
 }
