92f70c1c6038ddbdc74c86eebecdb70895971ca3
Castro <luisc@Castro-Desktop>
2020-09-18 21:54:53 +0100

    numCliente + numCidadao + nome + morada + profissao + telefone + email + numAgencia

--- ProjectoBancoRumos02/src/com/banco/clientes/Cliente.java
+++ ProjectoBancoRumos02/src/com/banco/clientes/Cliente.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.banco.clientes;
 
-/**
- *
- * @author luisc
- */
+import com.banco.Agencia;
+
 public class Cliente {
-    
+
+    private int numCliente;
+    private long numCidadao;
+    private String nome;
+    private String morada;
+    private String profissao;
+    private String telefone;
+    private String email;
+    private int numAgencia;
 }
