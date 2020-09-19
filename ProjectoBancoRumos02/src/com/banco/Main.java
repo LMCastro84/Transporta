ae463d3cb176b318dbbc9a58f62b004901d62c8e
Castro <luisc@Castro-Desktop>
2020-09-19 14:52:24 +0100

    public static void main (String[] args{} + agenciaPorto + agenciaLisboa + add to agencias

--- ProjectoBancoRumos02/src/com/banco/Main.java
+++ ProjectoBancoRumos02/src/com/banco/Main.java
@@ -1,6 +1,14 @@
 package com.banco;
 
 public class Main {
-    
-    Banco banco = new Banco();
+
+    public static void main(String[] args) {
+
+        Banco banco = new Banco();
+        Agencia agencia = new Agencia();
+        Agencia agenciaPorto = new Agencia(1, "Avenida da Boavista", banco.clientesPorto);
+        Agencia agenciaLisboa = new Agencia(2, "Avenida da Liberdade", banco.clientesLisboa);
+        agencia.agencias.add(agenciaPorto);
+        agencia.agencias.add(agenciaLisboa);
+    }
 }
