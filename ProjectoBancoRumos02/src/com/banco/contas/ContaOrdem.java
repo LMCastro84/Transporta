82ecd23c1b133772127d26941c6f03ba7c2b658f
Castro <luisc@Castro-Desktop>
2020-09-20 21:12:03 +0100

    delete Cliente

--- ProjectoBancoRumos02/src/com/banco/contas/ContaOrdem.java
+++ ProjectoBancoRumos02/src/com/banco/contas/ContaOrdem.java
@@ -4,8 +4,10 @@ import com.banco.clientes.Cliente;
 
 public class ContaOrdem extends Conta {
 
-    public ContaOrdem(int numConta, Cliente cliente, double saldo) {
-        super(numConta, cliente, saldo);
+    public ContaOrdem(int numConta, double saldo) {
+        super(numConta, saldo);
     }
+
+
 
 }
