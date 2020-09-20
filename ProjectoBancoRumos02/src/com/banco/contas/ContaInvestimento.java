c6103a8210187dedbac637f7dcc142489bb27b01
Castro <luisc@Castro-Desktop>
2020-09-20 21:25:05 +0100

    delete Cliente

--- ProjectoBancoRumos02/src/com/banco/contas/ContaInvestimento.java
+++ ProjectoBancoRumos02/src/com/banco/contas/ContaInvestimento.java
@@ -4,8 +4,8 @@ import com.banco.clientes.Cliente;
 
 public class ContaInvestimento extends Conta {
 
-    public ContaInvestimento(int numConta, Cliente cliente, double saldo) {
-        super(numConta, cliente, saldo);
+    public ContaInvestimento(int numConta, double saldo) {
+        super(numConta, saldo);
     }
 
 }
