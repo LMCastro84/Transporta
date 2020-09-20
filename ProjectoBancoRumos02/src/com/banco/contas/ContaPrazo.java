612c523d4b42be9f0d3b45b4609e2aa4a661219f
Castro <luisc@Castro-Desktop>
2020-09-20 21:25:55 +0100

    delete Cliente

--- ProjectoBancoRumos02/src/com/banco/contas/ContaPrazo.java
+++ ProjectoBancoRumos02/src/com/banco/contas/ContaPrazo.java
@@ -4,8 +4,8 @@ import com.banco.clientes.Cliente;
 
 public class ContaPrazo extends Conta {
 
-    public ContaPrazo(int numConta, Cliente cliente, double saldo) {
-        super(numConta, cliente, saldo);
+    public ContaPrazo(int numConta, double saldo) {
+        super(numConta, saldo);
     }
 
 }
