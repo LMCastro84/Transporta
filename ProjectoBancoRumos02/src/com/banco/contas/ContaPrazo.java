497594f759ea4e00db915219f8ee2d9f8b6d109a
Castro <luisc@Castro-Desktop>
2020-09-20 18:18:17 +0100

    Constructor

--- ProjectoBancoRumos02/src/com/banco/contas/ContaPrazo.java
+++ ProjectoBancoRumos02/src/com/banco/contas/ContaPrazo.java
@@ -1,9 +1,11 @@
 package com.banco.contas;
 
+import com.banco.clientes.Cliente;
+
 public class ContaPrazo extends Conta {
 
-    public ContaPrazo(int numConta, double saldo) {
-        super(numConta, saldo);
+    public ContaPrazo(int numConta, Cliente cliente, double saldo) {
+        super(numConta, cliente, saldo);
     }
 
 }
