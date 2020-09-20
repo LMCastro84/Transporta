3bd401193678760490814979c40cc1a81a13717d
Castro <luisc@Castro-Desktop>
2020-09-20 18:17:04 +0100

    Constructor

--- ProjectoBancoRumos02/src/com/banco/contas/ContaInvestimento.java
+++ ProjectoBancoRumos02/src/com/banco/contas/ContaInvestimento.java
@@ -1,9 +1,11 @@
 package com.banco.contas;
 
+import com.banco.clientes.Cliente;
+
 public class ContaInvestimento extends Conta {
 
-    public ContaInvestimento(int numConta, double saldo) {
-        super(numConta, saldo);
+    public ContaInvestimento(int numConta, Cliente cliente, double saldo) {
+        super(numConta, cliente, saldo);
     }
 
 }
