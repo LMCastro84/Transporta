12b73e61301cec6b86eb9b5523e9524397573751
Castro <luisc@Castro-Desktop>
2020-09-23 22:38:54 +0100

    tests

--- ProjectoBancoRumos02/src/com/banco/clientes/ClienteNormal.java
+++ ProjectoBancoRumos02/src/com/banco/clientes/ClienteNormal.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 public class ClienteNormal extends Cliente {
 
+    public ClienteNormal() {
+    }
+
     public ClienteNormal(int numCliente, long numCidadao, String nome, String morada, String profissao, String telefone, String email, int numAgencia, List<Conta> contas, List<Cartao> cartoes) {
         super(numCliente, numCidadao, nome, morada, profissao, telefone, email, numAgencia, contas, cartoes);
     }
