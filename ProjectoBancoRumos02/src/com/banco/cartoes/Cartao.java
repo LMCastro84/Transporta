eb1bac7718c81b9b5618fb2d55497d46efbbb4de
Castro <luisc@Castro-Desktop>
2020-09-23 21:26:32 +0100

    novoCartaoDebito()

---
+++ ProjectoBancoRumos02/src/com/banco/cartoes/Cartao.java
@@ -0,0 +1,48 @@
+package com.banco.cartoes;
+
+import com.banco.operacoes.Operacoes;
+
+public class Cartao implements Operacoes {
+
+    int numCartao;
+    public static int contadorCartoes = 0;
+
+    public Cartao(int numCartao) {
+        this.numCartao = numCartao;
+    }
+
+    public void setNumCartao(int numCartao) {
+        this.numCartao = numCartao;
+    }
+
+    public int getNumCartao() {
+        return numCartao;
+    }
+    
+    public static Cartao novoCartaoDebito(){
+        contadorCartoes++;
+        int numCartao = contadorCartoes;
+        return new CartaoDebito(numCartao);
+    }
+
+    @Override
+    public void depositar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void levantar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void transferir() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mostrarSaldo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
