cdfc1dcb18edf567be38131c07435ad0e9fdbe95
Castro <luisc@Castro-Desktop>
2020-09-23 20:23:20 +0100

    Constructor

--- ProjectoBancoRumos02/src/com/banco/clientes/ClienteNormal.java
+++ ProjectoBancoRumos02/src/com/banco/clientes/ClienteNormal.java
@@ -1,12 +1,14 @@
 package com.banco.clientes;
 
+import com.banco.cartoes.CartaoBancario;
 import com.banco.contas.Conta;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteNormal extends Cliente {
 
-    public ClienteNormal(int numCliente, long numCidadao, String nome, String morada, String profissao, String telefone, String email, int numAgencia, List<Conta> contas, int numCartao) {
-        super(numCliente, numCidadao, nome, morada, profissao, telefone, email, numAgencia, contas, numCartao);
+    public ClienteNormal(int numCliente, long numCidadao, String nome, String morada, String profissao, String telefone, String email, int numAgencia, ArrayList<Conta> contas, ArrayList<CartaoBancario> cartoes) {
+        super(numCliente, numCidadao, nome, morada, profissao, telefone, email, numAgencia, contas, cartoes);
     }
 
 }
