1c5d0e879e93b5a63596d81706fac5fa3231ba39
Castro <luisc@Castro-Desktop>
2020-09-23 21:46:31 +0100

    tests

--- ProjectoBancoRumos02/src/com/banco/cartoes/CartaoDebito.java
+++ ProjectoBancoRumos02/src/com/banco/cartoes/CartaoDebito.java
@@ -1,6 +1,6 @@
 package com.banco.cartoes;
 
-public class CartaoDebito extends CartaoBancario {
+public class CartaoDebito extends Cartao {
 
     public CartaoDebito(int numCartao) {
         super(numCartao);
