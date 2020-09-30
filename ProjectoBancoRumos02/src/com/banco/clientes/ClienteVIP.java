c6a4f91b09130e68370053338c4156b6caad4dc8
Castro <luisc@Castro-Desktop>
2020-09-30 23:20:51 +0100

    Tests

--- ProjectoBancoRumos02/src/com/banco/clientes/ClienteVIP.java
+++ ProjectoBancoRumos02/src/com/banco/clientes/ClienteVIP.java
@@ -2,23 +2,13 @@ package com.banco.clientes;
 
 import com.banco.cartoes.Cartao;
 import com.banco.contas.Conta;
+import com.banco.output.Menu;
 import java.util.List;
 
 public class ClienteVIP extends Cliente {
 
-    private String gestorConta;
-
     public ClienteVIP(int numCliente, long numCidadao, String nome, String morada, String profissao, String telefone, String email, int numAgencia, List<Conta> contas, List<Cartao> cartoes, String gestorConta) {
-        super(numCliente, numCidadao, nome, morada, profissao, telefone, email, numAgencia, contas, cartoes);
-        this.gestorConta = gestorConta;
-    }
-
-    public void setGestorConta(String gestorConta) {
-        this.gestorConta = gestorConta;
-    }
-
-    public String getGestorConta() {
-        return gestorConta;
+        super(numCliente, numCidadao, nome, morada, profissao, telefone, email, numAgencia, contas, cartoes, gestorConta);
     }
 
 }
