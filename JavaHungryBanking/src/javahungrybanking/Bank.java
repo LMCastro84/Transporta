d48507286efbead6ec4168b5cd2d301224d30b3f
Castro <luisc@Castro-Desktop>
2021-01-02 19:25:54 +0000

    criacao do objecto "conta1"

--- JavaHungryBanking/src/javahungrybanking/Bank.java
+++ JavaHungryBanking/src/javahungrybanking/Bank.java
@@ -1,6 +1,7 @@
 package javahungrybanking;
 public class Bank {
     public static void main(String[] args){
-        
+        BankAccount conta1 = new BankAccount("Quim", "Clt01");
+        conta1.mostrarMenu();
     }
 }
