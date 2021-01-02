b5fbd822ac2c2843805c12b500b33a053f474ab5
Castro <luisc@Castro-Desktop>
2021-01-02 19:17:13 +0000

    criacao switch

--- JavaHungryBanking/src/javahungrybanking/BankAccount.java
+++ JavaHungryBanking/src/javahungrybanking/BankAccount.java
@@ -14,8 +14,6 @@ public class BankAccount {
         this.idCliente = idCliente;
     }
 
-    
-    
     void depositar(int quantia) {
         if (quantia != 0) {
             saldo = saldo + quantia;
@@ -43,5 +41,60 @@ public class BankAccount {
     void mostrarMenu() {
         char opcao = '0';
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Benvindo " + nomeCliente);
+        System.out.println("O seu num de cliente e " + idCliente);
+        System.out.println("");
+        System.out.println("A- Conslutar saldo");
+        System.out.println("B- Depositar");
+        System.out.println("C- Levantar");
+        System.out.println("D- Ultima Transacao");
+        System.out.println("E- Sair");
+
+        do {
+            System.out.println("----------");
+            System.out.println("Introduzir opcao");
+            System.out.println("----------");            
+            opcao = scanner.next().charAt(0);
+            System.out.println("");
+            
+            switch(opcao){
+                case 'A':
+                    System.out.println("----------");
+                    System.out.println("Saldo = "+saldo);
+                    System.out.println("----------");
+                    System.out.println("");
+                    break;
+                    
+                case 'B':
+                    System.out.println("----------");
+                    System.out.println("Introduzir valor a depositar");
+                    System.out.println("----------");
+                    int quantia = scanner.nextInt();
+                    depositar(quantia);
+                    System.out.println("");
+                    break;
+                    
+                case 'C':
+                    System.out.println("----------");
+                    System.out.println("Introduzir valor a levantar");
+                    System.out.println("----------");
+                    int quantia2 = scanner.nextInt();
+                    levantar(quantia2);
+                    System.out.println("");
+                    break;
+                    
+                case 'D':
+                    System.out.println("----------");
+                    getUltimaTransacao();
+                    System.out.println("----------");
+                    System.out.println("");
+                    break;
+            }
+
+        }while (opcao != 'E');
+        
+        System.out.println("Obrigado por utilizar os nossos servicos!");
     }
+
 }
