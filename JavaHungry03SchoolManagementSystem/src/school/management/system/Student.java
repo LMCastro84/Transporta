afe1795bf7b5b4921684f53e4565205a8725413c
Castro <luisc@Castro-Desktop>
2021-01-05 23:14:55 +0000

    Create Constructor

--- JavaHungry03SchoolManagementSystem/src/school/management/system/Student.java
+++ JavaHungry03SchoolManagementSystem/src/school/management/system/Student.java
@@ -14,15 +14,19 @@ public class Student {
     private int feesTotal;
 
     /**
-     *
+     *Fees for every student: 30000
+     * Fees paid initially - 0
      * @param id student id - unique
      * @param name student name
      * @param grade student grade
      */
     public Student(int id, String name, int grade) {
+        feesPaid = 0;
+        feesTotal = 30000;
         this.id = id;
         this.name = name;
         this.grade = grade;
     }
+
 
 }
