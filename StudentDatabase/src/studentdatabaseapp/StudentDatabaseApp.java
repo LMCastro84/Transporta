ea44cb485da43a8fe535b02950c2e5f5c6396b08
Castro <luisc@Castro-Desktop>
2021-01-08 22:36:02 +0000

    First instructions to fulfill

--- StudentDatabase/src/studentdatabaseapp/StudentDatabaseApp.java
+++ StudentDatabase/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,6 +1,10 @@
 package studentdatabaseapp;
+
 public class StudentDatabaseApp {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+        //Ask how many users to add
         
+        //Create number of students
     }
 }
