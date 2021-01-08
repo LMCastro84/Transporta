a6ec2ed5215b98fbba91859e28fc18233c6dc18c
Castro <luisc@Castro-Desktop>
2021-01-08 22:45:55 +0000

    Constructor

--- StudentDatabase/src/studentdatabaseapp/Student.java
+++ StudentDatabase/src/studentdatabaseapp/Student.java
@@ -1,22 +1,33 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
+
     private String firstName;
     private String lastName;
-    private String gradeYear;
+    private int gradeYear;
     private int studentID;
     private String courses;
     private int tuitionBalance;
     private double costOfCourse = 600;
+
     //Constructor with name and year
-    
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+        this.firstName = in.nextLine();
+        System.out.println("Enter student last name: ");
+        this.lastName = in.nextLine();
+        System.out.println("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\n"
+                + "Enter student class level: ");
+        this.gradeYear = in.nextInt();
+        System.out.println(firstName + " "+lastName+" - "+gradeYear+".");
+    }
     //Generate ID
-    
+
     //Enrol in courses
-    
     //View balance
-    
     //Pay Tuition
-    
     //Show status
 }
