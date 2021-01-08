e22a3a75d7b1f3921e8030b307984d0dd19bb7db
Castro <luisc@Castro-Desktop>
2021-01-08 22:37:25 +0000

    First instructions to fulfill

--- StudentDatabase/src/studentdatabaseapp/Student.java
+++ StudentDatabase/src/studentdatabaseapp/Student.java
@@ -1,4 +1,15 @@
 package studentdatabaseapp;
+
 public class Student {
+    //Constructor with name and year
     
+    //Generate ID
+    
+    //Enrol in courses
+    
+    //View balance
+    
+    //Pay Tuition
+    
+    //Show status
 }
