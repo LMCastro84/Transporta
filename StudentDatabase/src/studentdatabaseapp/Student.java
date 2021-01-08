779d828ddfd38da1f6a74bf044c2fbd05d098229
Castro <luisc@Castro-Desktop>
2021-01-08 22:39:36 +0000

    Variables

--- StudentDatabase/src/studentdatabaseapp/Student.java
+++ StudentDatabase/src/studentdatabaseapp/Student.java
@@ -1,6 +1,13 @@
 package studentdatabaseapp;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private double costOfCourse = 600;
     //Constructor with name and year
     
     //Generate ID
