905098d8cdaee3ec979b6865d6aa5fdbff9421bb
Castro <luisc@Castro-Desktop>
2021-01-09 23:00:34 +0000

    Ask how many students to create

--- StudentDatabase/src/studentdatabaseapp/StudentDatabaseApp.java
+++ StudentDatabase/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,5 +1,7 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
     public static void main(String[] args) {
@@ -7,8 +9,19 @@ public class StudentDatabaseApp {
         stu1.enroll();
         stu1.payTuition();
         System.out.println(stu1.toString());
-        //Ask how many users to add
+        
+        //Ask how many students to add
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
         
         //Create number of students
+        for (int i = 0; i < numOfStudents; i++){
+            students[i] = new Student();
+            students[i].enroll();
+            students[i].payTuition();
+            System.out.println(students[i].toString());
+        }
     }
 }
