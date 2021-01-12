745f8420902b9b6ff952d6d938d6bd35a4eb9c66
Castro <luisc@Castro-Desktop>
2021-01-12 22:56:40 +0000

    Constructor

--- JavaHungry05ATM/src/javahungry05atm/User.java
+++ JavaHungry05ATM/src/javahungry05atm/User.java
@@ -8,25 +8,33 @@ public class User {
      * First name of user
      */
     private String firstName;
-    
+
     /**
      * Last name of user
-     */    
+     */
     private String lastName;
-    
+
     /**
      * ID number of user
      */
     private String uuid; //Unique Universal Identifier
-    
+
     /**
      * MD5 hash (algorithm) of user's pin number
-     */    
+     */
     private byte pinHash[];
-    
+
     /**
      * List of accounts for user
      */
     private ArrayList<Account> accounts;
 
+    public User(String firstName, String lastName, String pin, Bank theBank) {
+        //set user's name
+        this.firstName = firstName;
+        this.lastName = lastName;
+        
+        //store pin's MD5 hash, rather than the original value,for security reasons
+        
+    }
 }
