bcb00f55a8f524ee335620ff58c644a3dcc2b2b5
Castro <luisc@Castro-Desktop>
2021-01-12 22:43:06 +0000

    Comments

--- JavaHungry05ATM/src/javahungry05atm/User.java
+++ JavaHungry05ATM/src/javahungry05atm/User.java
@@ -8,18 +8,25 @@ public class User {
      * First name of user
      */
     private String firstName;
+    
     /**
      * Last name of user
-     */
+     */    
     private String lastName;
+    
     /**
      * ID number of user
      */
     private String uuid; //Unique Universal Identifier
+    
     /**
-     * 
-     */
+     * MD5 hash (algorithm) of user's pin number
+     */    
     private byte pinHash[];
+    
+    /**
+     * List of accounts for user
+     */
     private ArrayList<Account> accounts;
 
 }
