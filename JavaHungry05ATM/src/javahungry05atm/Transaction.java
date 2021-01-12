4055afbadfc297ff47d1189fa859f5907eefdcee
Castro <luisc@Castro-Desktop>
2021-01-12 22:49:26 +0000

    Variables + Comments

--- JavaHungry05ATM/src/javahungry05atm/Transaction.java
+++ JavaHungry05ATM/src/javahungry05atm/Transaction.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javahungry05atm;
 
-/**
- *
- * @author luisc
- */
+import java.util.Date;
+
 public class Transaction {
+    /**
+     * Amount for this transaction
+     */
+    private double amount;
     
+    /**
+     * Tima and date of transaction
+     */
+    private Date timestamp;
+    
+    /**
+     * Memo for transaction
+     */
+    private String memo;
+    
+    /**
+     * Account in wich the transaction was performed
+     */
+    private Account inAccount;
 }
