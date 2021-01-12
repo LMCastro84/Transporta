2a17aca63d3f5b2ced2d7df3fdcdf370596a6c3d
Castro <luisc@Castro-Desktop>
2021-01-12 23:22:34 +0000

    Constructor

--- JavaHungry05ATM/src/javahungry05atm/Account.java
+++ JavaHungry05ATM/src/javahungry05atm/Account.java
@@ -8,24 +8,40 @@ public class Account {
      * Name of the account
      */
     private String name;
-    
+
     /**
      * Current balance
      */
     private double balance;
-    
+
     /**
      * ID number
      */
     private String uuid;
-    
+
     /**
      * User object that uses this account
      */
     private User holder;
-    
+
     /**
      * List of transactions for this account
      */
     private ArrayList<Transaction> transactions;
+
+    public Account(String name, User holder, Bank theBank){
+        //set the account name and holder
+        this.name = name;
+        this.holder = holder;
+        
+        //get new account UUID
+        this.uuid = theBank.getNewAccountUUID();
+        
+        //initialize transactions
+        this.transactions = new ArrayList<Transaction>();
+        
+        //add to holder and bank lists
+        holder.addAccount(this);
+        theBank.addAccount(this);
+    }
 }
