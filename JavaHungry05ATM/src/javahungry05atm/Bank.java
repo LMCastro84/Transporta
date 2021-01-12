f63777d4771ae87a21e9608d09ebc04c599a8d69
Castro <luisc@Castro-Desktop>
2021-01-12 23:23:26 +0000

    getNewUserUUID() + getNewAccountUUID() + addAccount()

--- JavaHungry05ATM/src/javahungry05atm/Bank.java
+++ JavaHungry05ATM/src/javahungry05atm/Bank.java
@@ -3,19 +3,31 @@ package javahungry05atm;
 import java.util.ArrayList;
 
 public class Bank {
-    
+
     /**
      * Name of Bank
      */
     private String name;
-    
+
     /**
      * List of users
      */
     private ArrayList<User> users;
-    
+
     /**
      * List of accounts
      */
     private ArrayList<Account> accounts;
+
+    public String getNewUserUUID() {
+
+    }
+
+    public String getNewAccountUUID() {
+
+    }
+
+    void addAccount(Account aThis) {
+
+    }
 }
