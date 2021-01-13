6f44b36c74104949fd3d42fd32ea366704830d80
Castro <luisc@Castro-Desktop>
2021-01-13 22:08:23 +0000

    2 Constructors (Overloading) + comments

--- JavaHungry05ATM/src/javahungry05atm/Transaction.java
+++ JavaHungry05ATM/src/javahungry05atm/Transaction.java
@@ -3,23 +3,49 @@ package javahungry05atm;
 import java.util.Date;
 
 public class Transaction {
+
     /**
      * Amount for this transaction
      */
     private double amount;
-    
+
     /**
      * Tima and date of transaction
      */
     private Date timestamp;
-    
+
     /**
      * Memo for transaction
      */
     private String memo;
-    
+
     /**
      * Account in wich the transaction was performed
      */
     private Account inAccount;
+
+    /**
+     * Create new transaction
+     * @param amount    amount transacted
+     * @param inAccount account the transaction belongs to
+     */
+    public Transaction(double amount, Account inAccount) {
+        this.amount = amount;
+        this.inAccount = inAccount;
+        this.timestamp = new Date();
+        this.memo = "";
+    }
+
+    /**
+     * Create new transaction
+     * @param amount    amount transacted
+     * @param memo      memo of transaction
+     * @param inAccount account the transaction belongs to
+     */
+    public Transaction(double amount, String memo, Account inAccount) {
+        //call the upper two-arg constructor first
+        this(amount, inAccount);
+        //set the memo
+        this.memo = memo;
+    }
 }
