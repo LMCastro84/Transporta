531bcbf83f71778cd29b4ddaab92293e304f16f1
Castro <luisc@Castro-Desktop>
2021-01-14 22:45:59 +0000

    Tests

--- JavaHungry05ATM/src/javahungry05atm/ATM.java
+++ JavaHungry05ATM/src/javahungry05atm/ATM.java
@@ -1,7 +1,23 @@
 package javahungry05atm;
-public class ATM{ 
+
+import java.util.Scanner;
+
+public class ATM {
+
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        
+        //create Bank
+        Bank theBank = new Bank("Foz Bank");
+        
+        // add a user, and create a savings account
+        User user = theBank.addUser("Luis", "Castro", "1234");
+        
+        //add a checking account for user
+        Account newAccount = new Account("Checking", user, theBank);
+        user.addAccount(newAccount);
+        theBank.addAccount(newAccount);
     }
-    
+
 }
