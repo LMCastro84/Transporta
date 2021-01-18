b93a7a7a36bc99c6ce6d8773fab17875b207b92a
Castro <luisc@Castro-Desktop>
2021-01-18 22:36:02 +0000

    getSummaryLine() + comments

--- JavaHungry05ATM/src/javahungry05atm/Transaction.java
+++ JavaHungry05ATM/src/javahungry05atm/Transaction.java
@@ -59,4 +59,19 @@ public class Transaction {
     public double getAmount() {
         return this.amount;
     }
+
+    /**
+     * Get a string summarizing the transaction
+     *
+     * @return the summary string
+     */
+    public String getSummaryLine() {
+        if (this.amount >= 0) {
+            return String.format("%s : %.02f€ : %s", this.timestamp.toString(),
+                    this.amount, this.memo);
+        } else {
+            return String.format("%s : (%.02f)€ : %s", this.timestamp.toString(),
+                    this.amount, this.memo);
+        }
+    }
 }
