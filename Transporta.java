6dbce5ed843a7a4dccf0346fdd4a923b02067fdc
Luis Miguel Castro <58310131+LMCastro84@users.noreply.github.com>
2021-01-20 12:54:35 +0000

    Update Transporta.java

--- Transporta.java
+++ Transporta.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Transporta {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		Hospital hospitalFoz = new Hospital("da Foz", "Porto", "Norte");
+		Doente doente1 = new Doente(in.nextLine());
 	}
 }
