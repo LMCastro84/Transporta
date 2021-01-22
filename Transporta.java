55e06f29aa4d0347192e2e72b29bedbc0716b622
Luis Miguel Castro <58310131+LMCastro84@users.noreply.github.com>
2021-01-22 11:39:25 +0000

    Update Transporta.java

--- Transporta.java
+++ Transporta.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 
 public class Transporta {
-	public static void main(String[] args) {
+	
+	public static void criaDoente (){
 		Scanner in = new Scanner(System.in);
-		Hospital hospitalFoz = new Hospital("da Foz", "Porto", "Norte");
 		System.out.print("Introduza o nome do novo Doente: ");
-		Doente doente1 = new Doente(in.nextLine());
+		Doente novoDoente = new Doente(in.nextLine());
+		
+}
+	public static void main(String[] args) {
+		Hospital hospitalFoz = new Hospital("da Foz", "Porto", "Norte");
+		criaDoente();
+		
+		
 	}
 }
